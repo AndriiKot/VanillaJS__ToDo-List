56f1662dfb27de348b745ec8f2b12084b2b1aa18
AndriiKot <AndriiKot@users.noreply.github.com>
2025-06-20 20:37:50 +0200

    VanillaJS(TODO List)  **feat(js):  create function addTask and vailidateValue

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,5 +2,59 @@
 
 import { getTodoElements } from "./dom.js";
 
-const { todoInput, todoList, todoButton } = getTodoElements();
-console.log("From getTodoElements:", todoInput, todoList, todoButton);
+window.addEventListener("DOMContentLoaded", () => {
+  const { todoInput, todoList, todoButton } = getTodoElements();
+
+  todoButton.addEventListener("click", () => {
+    addTask(todoInput, todoList);
+  });
+
+  todoInput.focus();
+});
+
+const addTask = (input, list) => {
+  const task = validateInput(input);
+  if (task) {
+    const todoItem = createTodoItem(task);
+    appendTodoItem(list, todoItem);
+  }
+  clearInput(input);
+};
+
+const validateInput = (input) => {
+  const task = input.value.trim();
+  if (task === "") {
+    showError("Task can't be empty");
+    input.focus();
+    return;
+  }
+  showError(""); // очистить ошибку
+  return task;
+};
+
+const clearInput = (input) => {
+  input.value = "";
+};
+
+const createTodoItem = (taskText) => {
+  const li = document.createElement("li");
+  li.className = "todo__item";
+  li.textContent = taskText;
+  return li;
+};
+
+const appendTodoItem = (list, item) => {
+  list.appendChild(item);
+};
+
+const showError = (message) => {
+  let error = document.querySelector(".todo__error");
+  if (!error) {
+    error = document.createElement("p");
+    error.className = "todo__error";
+    error.style.color = "red";
+    error.style.marginTop = "8px";
+    document.querySelector(".todo__box").appendChild(error);
+  }
+  error.textContent = message;
+};

879efeacb9369af0719f44a8f22a4c4fd6ef30cd
AndriiKot <AndriiKot@users.noreply.github.com>
2025-06-26 12:50:46 +0200

    VanillaJS(TODO List)  **build(vite.config):  update config vite

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
 import { defineConfig } from "vite";
-import path from "node:path";
+import { aliases } from "./path-aliases.js";
 
 export default defineConfig({
   root: "app",
   resolve: {
-    alias: {
-      "@app": path.resolve(__dirname, "app"),
-      "@scripts": path.resolve(__dirname, "app/scripts"),
-      "@ui": path.resolve(__dirname, "app/scripts/ui"),
-      "@asserts": path.resolve(__dirname, "app/scripts/asserts"),
-      "@handlers": path.resolve(__dirname, "app/scripts/handlers"),
-      "@tests": path.resolve(__dirname, "app/tests"),
-      "@test-utils": path.resolve(__dirname, "app/tests/test-utils"),
-      "@utils": path.resolve(__dirname, "app/scripts/utils.js"),
-      "@main": path.resolve(__dirname, "app/scripts/main.js"),
-      "@task": path.resolve(__dirname, "app/scripts/task.js"),
-      "@selectors": path.resolve(__dirname, "app/scripts/selectors.js"),
-      "@html": path.resolve(__dirname, "app/index.html"),
-      "@assets": path.resolve(__dirname, "app/assets"),
-      "@constants": path.resolve(__dirname, "app/scripts/constants"),
-      "@VALID_HTML_TAGS": path.resolve(
-        __dirname,
-        "app/scripts/constants/VALID_HTML_TAGS.js"
-      ),
-    },
+    alias: aliases,
   },
 });

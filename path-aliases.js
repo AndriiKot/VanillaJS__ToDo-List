490e6f0dbea6c7e8786d24c8bf154ba53d4d3c6e
AndriiKot <AndriiKot@users.noreply.github.com>
2025-06-26 12:47:34 +0200

    VanillaJS(TODO List)  **build(path-alises):  create path-aliases.js

---
+++ path-aliases.js
@@ -0,0 +1,21 @@
+import path from "node:path";
+
+const root = path.resolve(__dirname, "app");
+
+export const aliases = {
+  "@app": root,
+  "@scripts": path.join(root, "scripts"),
+  "@ui": path.join(root, "scripts/ui"),
+  "@asserts": path.join(root, "scripts/asserts"),
+  "@handlers": path.join(root, "scripts/handlers"),
+  "@tests": path.join(root, "tests"),
+  "@test-utils": path.join(root, "tests/test-utils"),
+  "@utils": path.join(root, "scripts/utils.js"),
+  "@main": path.join(root, "scripts/main.js"),
+  "@task": path.join(root, "scripts/task.js"),
+  "@selectors": path.join(root, "scripts/selectors.js"),
+  "@html": path.join(root, "index.html"),
+  "@assets": path.join(root, "assets"),
+  "@constants": path.join(root, "scripts/constants"),
+  "@VALID_HTML_TAGS": path.join(root, "scripts/constants/VALID_HTML_TAGS.js"),
+};

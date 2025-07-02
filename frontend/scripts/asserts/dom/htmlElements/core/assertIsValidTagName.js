c8a717b73528b69c37ce59a43cea68af1fa16d69
AndriiKot <AndriiKot@users.noreply.github.com>
2025-07-02 08:45:39 +0200

    VanillaJS(TODO List)  **refactor(unit-test):  assertIsValidStandardTagName.js test 2

---
+++ frontend/scripts/asserts/dom/htmlElements/core/assertIsValidTagName.js
@@ -0,0 +1,14 @@
+import { isEmpty } from "@utils";
+import { VALID_STANDARD_HTML_TAGS } from "@ui";
+import { assertIsString } from "frontend/scripts/asserts/primitives";
+
+export const assertIsValidStandardTagName = (tagName) => {
+  assertIsString(tagName);
+  if (isEmpty(tagName)) {
+    throw new Error("Tag name must not be empty");
+  }
+
+  if (!VALID_STANDARD_HTML_TAGS.includes(tagName)) {
+    throw new Error(`Invalid Standard tag name: "${tagName}"`);
+  }
+};

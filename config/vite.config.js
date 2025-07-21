d628ae2c2d94abd6b94ae4e0a821177a859df329
AndriiKot <AndriiKot@users.noreply.github.com>
2025-07-21 20:59:54 +0200

    VanillaJS(TODO List)  **feat(vite/config):  update vite config

--- config/vite.config.js
+++ config/vite.config.js
@@ -2,8 +2,19 @@ import { defineConfig } from "vite";
 import { aliases } from "./path-aliases.js";
 
 export default defineConfig({
+  base: './',
   root: "frontend",
   resolve: {
     alias: aliases,
   },
+  build: {
+    outDir: '../dist',           
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  }, 
 });
+
+

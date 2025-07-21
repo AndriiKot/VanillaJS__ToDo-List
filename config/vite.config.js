afc61362c5acf2346cb581ae2e7ece4a43436419
AndriiKot <AndriiKot@users.noreply.github.com>
2025-07-21 21:22:59 +0200

    VanillaJS(TODO List)  **feat(PostCSS):  add PostCSS

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import { aliases } from "./path-aliases.js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   base: './',
   root: "frontend",
   resolve: {
     alias: aliases,
+  },
+  css: {
+    postcss: path.resolve(__dirname, "postcss.config.js"),
   },
   build: {
     outDir: '../dist',           
@@ -16,5 +24,3 @@ export default defineConfig({
     open: true,
   }, 
 });
-
-

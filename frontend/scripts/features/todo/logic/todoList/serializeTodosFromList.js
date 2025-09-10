1d01ce583143592a9619fe430952ac8655c7732a
AndriiKot <AndriiKot@users.noreply.github.com>
2025-09-10 08:37:56 +0200

    VanillaJS(TODO List)  **refactor(todo/logic):  serializeTodosFromList.js refactor

--- frontend/scripts/features/todo/logic/todoList/serializeTodosFromList.js
+++ frontend/scripts/features/todo/logic/todoList/serializeTodosFromList.js
@@ -1,9 +1,10 @@
 import { getTodoItemCheckedClassName } from '@features';
+import { getTextContent } from '@ui';
 
 export const serializeTodosFromList = (todoList) => {
   return Array.from(todoList.children, (li) => {
     return {
-      text: li.querySelector('.todo__text').textContent,
+      text: getTextContent(li.querySelector('.todo__text')),
       checked: li.matches(`.${getTodoItemCheckedClassName()}`),
     };
   });

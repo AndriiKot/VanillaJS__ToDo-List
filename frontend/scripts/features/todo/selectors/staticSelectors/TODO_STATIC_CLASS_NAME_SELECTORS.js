40007317ec5da27eb8b3da902cab4331d4441584
AndriiKot <AndriiKot@users.noreply.github.com>
2025-09-10 07:01:10 +0200

    VanillaJS(TODO List)  **refactor(todo/selectors):  delete TODO_STATIC_CLASS_NAME_SELECTORS.js

--- frontend/scripts/features/todo/selectors/staticSelectors/TODO_STATIC_CLASS_NAME_SELECTORS.js
+++
@@ -1,6 +0,0 @@
-export const TODO_STATIC_CLASS_NAME_SELECTORS = {
-  todoButton: '.todo__btn',
-  todoInput: '.todo__input',
-  todoList: '.todo__list',
-  todoValidMessage: '.todo__error',
-};
--- frontend/scripts/features/todo/selectors/staticSelectors/index.js
+++ frontend/scripts/features/todo/selectors/staticSelectors/index.js
@@ -1,4 +1,3 @@
-export * from './TODO_STATIC_CLASS_NAME_SELECTORS';
 export * from './TODO_STATIC_DATA_SELECTORS';
 export * from './staticTodoElements';
 export * from './getStaticTodoElements.js';

75ad52d09be7728f7dbd3797770c2f9ada930847
AndriiKot <AndriiKot@users.noreply.github.com>
2025-09-16 10:26:59 +0200

    VanillaJS(TODO List)  **fix(todo/input) prevent default form submission on Enter key

--- frontend/scripts/features/todo/handlers/handleKeyDown/handleKeyDownTodo.js
+++ frontend/scripts/features/todo/handlers/handleKeyDown/handleKeyDownTodo.js
@@ -2,6 +2,7 @@ import { handleTodoTaskSubmission as addTodoTask } from '@features';
 
 export const handleKeyDownTodo = (input, list, validationMsg) => (e) => {
   if (e.key === 'Enter') {
+    e.preventDefault();
     addTodoTask(input, list, validationMsg);
   }
 };
